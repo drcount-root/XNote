cf082fe794b8bb7d7f3ee12187d66ca6b22061ee
drcount-root <about.subhamsahu@gmail.com>
2022-11-02 20:12:44 +0530

    02nd Nov 2022

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+{
+    "message": "Welcome to Thunder Client",
+    "about": "Thunder Client is a hand-crafted lightweight Rest API Client for VS Code.",
+    "createdBy": "Ranga Vadhineni",
+    "github": "github.com/rangav/thunder-client-support",
+    "twitter": "twitter.com/thunder_client",
+    "auth": "Supported Authentication methods Basic Auth, Bearer Token & OAuth 2.0",
+    "graphql": "The client supports Graphql Query & Variables",
+    "support": "For Bugs & Feature requests please submit on github",
+    "share": "Please spread the word about Thunder Client"
+  }
